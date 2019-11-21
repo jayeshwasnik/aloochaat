e0436405bc195999c5031bdcee2233af595787df
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-11-21 16:02:12 +0530

    1 file modified

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './../../app.service.ts';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 //for toastr
 import { ToastrService } from 'ngx-toastr';
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   public email;
   public password;
 
-  constructor(private _route: ActivatedRoute, private router: Router, public appService: AppService,private toastr:ToastrService) { }
+  constructor(private _route: ActivatedRoute,private cookie:Cookie, private router: Router, public appService: AppService,private toastr:ToastrService) { }
 
   ngOnInit() {
   }
@@ -30,8 +31,22 @@ public signinFunction(){
 let data={email:this.email,
           password:this.password}
 
-this.appService.userLogin(data).subscribe(data=>{this.toastr.success("Login Succesful");
-console.log(data)},
+this.appService.userLogin(data).subscribe(apiResponse=>{
+  if (apiResponse.status===200) 
+  {this.toastr.success("Login Succesful");}
+
+console.log(apiResponse);
+//for setting cookies
+Cookie.set('authToken',apiResponse.data.authToken);
+Cookie.set('receiverId', apiResponse.data.userDetails.userId);
+Cookie.set('receiverName', apiResponse.data.userDetails.firstName + ' ' + apiResponse.data.userDetails.lastName);
+//setting the data in local storage
+this.appService.setUserInfoInLocalStorage(apiResponse.data.userDetails);
+},
+
+
+
+
 error=>{this.toastr.error("there was an error")});
 
 
