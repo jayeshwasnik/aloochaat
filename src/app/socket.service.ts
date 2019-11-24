932cec2578442321a935d38a2f7eadf0c2d4b450
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-11-25 02:09:01 +0530

    1 file modified

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 
-import * as io from 'socket.io-client'
+import * as io from 'socket.io-client';
+import { Observable } from "rxjs/Observable";
+
+import {Cookie} from 'ng2-cookies/ng2-cookies';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/toPromise';
+
+//to use http params
+import{HttpHeaders,HttpParams} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'; 
 
 @Injectable()
 export class SocketService {
 
-  constructor() { }
+  private url='https://chatapi.edwisor.com';
+private socket;
+
+
+  constructor(public httpClient:HttpClient) {
+    this.socket=io.connect(this.url);
+   }
+
+
+   
 
 }
