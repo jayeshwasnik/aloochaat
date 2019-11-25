06e50d0f4d047fba81e7eef51480b4a1c3a50a6a
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-11-25 13:56:27 +0530

    1 file modified

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 
+//importing socket.io-client is being added
 import * as io from 'socket.io-client';
 import { Observable } from "rxjs/Observable";
 
@@ -20,10 +21,52 @@ private socket;
 
 
   constructor(public httpClient:HttpClient) {
+    //here the connection is created in socket io
     this.socket=io.connect(this.url);
    }
 
+    //listening to verify user event
+   public verifyUser=()=>{
+     //below we are creating a observable instance observer
+     return Observable.create((observer)=>{
+       //connecting the socket to verifyuser event,and executing callback function using the data we get from event;also data is just a random name for variable,it can be anything
+       this.socket.on('verifyUser',(data)=>{
+         //pushing the data to the subscribers using observer.next
+         observer.next(data);
 
+       })//end socket
+
+     });//end observable
+   }//end verify user
+
+
+//listening to onlineUserList event
+public onlineUserList=()=>{
+     //below we are creating a observable instance observer
+     return Observable.create((observer)=>{
+       //connecting the socket to verifyuser event,and executing callback function using the data we get from event
+       this.socket.on('online-user-list',(userList)=>{
+         //pushing the data to the subscribers using observer.next
+         observer.next(userList);
+
+       })//end socket
+
+     });//end observable
+   }//end function
+
+
+public disconnectedSocket=()=>{
+     //below we are creating a observable instance observer
+     return Observable.create((observer)=>{
+       //connecting the socket to verifyuser event,and executing callback function using the data we get from event
+       this.socket.on('disconnect',()=>{
+         //pushing the data to the subscribers using observer.next
+         observer.next();
+
+       })//end socket
+
+     });//end observable
+   }//end function 
    
 
 }
