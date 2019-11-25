297784b399aa2d72fa257114674d787bc728fa76
jayeshwasnik <jayesh.wasnik.ece12@itbhu.ac.in>
2019-11-25 16:54:19 +0530

    1 file modified

--- src/app/chat/chat-box/chat-box.component.ts
+++ src/app/chat/chat-box/chat-box.component.ts
@@ -15,10 +15,36 @@ import {CookieService} from 'ngx-cookie-service';
   styleUrls: ['./chat-box.component.css']
 })
 export class ChatBoxComponent implements OnInit {
+  public authToken:any;
+  public userInfo:any;
+  public recieverId:any;
+  public recieverName:any;
+  public userList:any[];
+  public disconnectedSocket:boolean;
 
-  constructor(private cookieService:CookieService;) { }
+  constructor(public cookieService:CookieService,public socketService:SocketService,public router:Router ) {
+    this.recieverId=this.cookieService.get('recieverId');
+    this.recieverName=this.cookieService.get('recieverName');
+    
+   }
 
   ngOnInit() {
+    this.authToken=this.cookieService.get('authToken');
+    this.userInfo=this.AppService.getUserInfoFromLocalStorage();
+
+    this.checkStatus();
+    this.verifyUserConfirmation();
+    this.getOnlineUserList();
   }
+
+
+public checkStatus:any=()=>{
+  if(this.cookieService.get('authToken')===undefined || this.cookieService.get('authToken')==='' || this.cookieService.get('authToken')===null){
+    this.router.navigate(['/']);
+    return false;
+  }else{
+return true;
+  }
+}
 
 }
